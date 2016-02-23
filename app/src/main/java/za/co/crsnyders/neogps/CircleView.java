b337ca95a6f7754809c875069385627d79bea324
Christopher Snyders <csnyders@thefieldoffice.co.za>
2016-02-23 11:35:59 +0200

    first commit

---
+++ app/src/main/java/za/co/crsnyders/neogps/CircleView.java
@@ -0,0 +1,35 @@
+package za.co.crsnyders.neogps;
+/**
+ * Created by csnyders on 2016/02/23.
+ */
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CircleView extends View {
+
+    private List<Dot> dots = new ArrayList<>();
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public void addDot(Dot dot){
+        this.dots.add(dot);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for(Dot dot : dots) {
+            Paint paint = new Paint();
+            paint.setColor(dot.colour);
+            canvas.drawCircle(dot.point.x, dot.point.y, 50, paint);
+        }
+    }
+
+}
---
+++ app/src/main/java/za/co/crsnyders/neogps/Dot.java
@@ -0,0 +1,17 @@
+package za.co.crsnyders.neogps;
+
+import android.graphics.Point;
+
+/**
+ * Created by csnyders on 2016/02/23.
+ */
+public class Dot {
+
+    public final int colour;
+    public final Point point;
+
+    public Dot(int colour, Point point){
+        this.colour = colour;
+        this.point = point;
+    }
+}
